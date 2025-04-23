deafdd7290e5fc963c6d96bc40292d5f5095ca77
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-04-23 18:44:19 -0300

    Add: setting templates dir

--- app/settings.py
+++ app/settings.py
@@ -191,5 +191,14 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+TEMPLATES = [
+    {
+        
+        'DIRS': [BASE_DIR / "templates"],  # se os templates estiverem fora das apps
+        
+        'APP_DIRS': True,  # se os templates estiverem dentro das apps
+    },
+]
