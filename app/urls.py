4683c06fa16a4140dfb9d4f3de44a58888f3348f
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-04-24 10:56:25 -0300

    Rename app auth > custom_auth

--- app/urls.py
+++ app/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('documents/', include('manager_documents.urls')),
-    path('auth/', include('auth.urls')),
+    path('auth/', include('custom_auth.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
