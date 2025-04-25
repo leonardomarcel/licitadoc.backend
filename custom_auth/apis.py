4581eaa0cf092ff0ce1a591c98c37eba3a0e8b6d
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-04-25 15:15:00 -0300

    Refactor: set user password on password_reset endpoint

--- custom_auth/apis.py
+++ custom_auth/apis.py
@@ -53,15 +53,15 @@ def check_groups(request):
 def password_reset(request):
     email = request.data.get('email')
     try:
-        User.objects.get(email=email)
+        user =  User.objects.get(email=email)
         password = generate_password()
         html_content = render_to_string('auth/email/password_reset.html', {
         'password': password,
         'email': email
         })
         send_email('[Licita.doc] Email de redefinição de senha', message="oi", from_email="licitadoc@mail.com", to_email=email, html_message=html_content)
-        # user.set_password(password)
-        # user.save()
+        user.set_password(password)
+        user.save()
         return JsonResponse({'success': True, 'message': 'Se estiver cadastrado, o email foi enviado.'})
     except Exception as e:
         return JsonResponse({'success': False, 'message': str(e)}) #Email enviado com sucesso'})
