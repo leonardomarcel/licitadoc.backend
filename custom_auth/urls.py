90cb09fa5c528c7c71164ff6a4da4f9defa6db56
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-05-28 19:06:51 -0300

    Add: using djangorestframework_simplejwt

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import apis
 
@@ -10,6 +14,8 @@ urlpatterns = [
     path('check-groups/', apis.check_groups, name='check_groups'),
     path('password-reset/', apis.password_reset, name='password_reset'),
     path('new-account-user/', apis.new_account_user, name='new_account_user'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     
 ]
--- manager_documents/urls.py
+++ manager_documents/urls.py
@@ -1,10 +1,4 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 
 from . import apis
 
@@ -13,9 +7,4 @@ router = DefaultRouter()
 router.register(r'', apis.DocumentViewSet, basename='document')
 router.register(r'tags', apis.TagViewSet, basename='tag')
 
-urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
-
-urlpatterns += router.urls
+urlpatterns = router.urls
