2662148690abbbb375958bc73eac55db14e125c5
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-05-28 15:52:22 -0300

    Refactor: changing function based view for class based view and make it more restifull then before

--- manager_documents/urls.py
+++ manager_documents/urls.py
@@ -1,8 +1,8 @@
-from rest_framework.routers import DefaultRouter
+from reest_framework.routers import DefaultRouter
 
 from . import apis
 
 
 router = DefaultRouter()
-router.register(r'document', apis.DocumentViewSet, basename='document')
-router.register(r'tag', apis.TagViewSet, basename='tag')
+router.register(r'documents', apis.DocumentViewSet, basename='document')
+router.register(r'tags', apis.TagViewSet, basename='tag')
