bb8dbe018a07a324070b724e8819ca15cb924d86
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-05-28 17:13:54 -0300

    Refacter: changing  path of apis url

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -4,12 +4,12 @@ from . import apis
 
 
 urlpatterns = [
-    path('api/login/', apis.login_view, name='login'),
-    path('api/logout/', apis.logout_view, name='logout'),
-    path('api/check-session/', apis.check_session, name='check_session'),
-    path('api/check-groups/', apis.check_groups, name='check_groups'),
-    path('api/password-reset/', apis.password_reset, name='password_reset'),
-    path('api/new-account-user/', apis.new_account_user, name='new_account_user'),
+    path('login/', apis.login_view, name='login'),
+    path('logout/', apis.logout_view, name='logout'),
+    path('check-session/', apis.check_session, name='check_session'),
+    path('check-groups/', apis.check_groups, name='check_groups'),
+    path('password-reset/', apis.password_reset, name='password_reset'),
+    path('new-account-user/', apis.new_account_user, name='new_account_user'),
 
     
 ]
