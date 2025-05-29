182eb5349f1f13ad8a6b1adda7655b2890253427
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-05-28 22:06:40 -0300

    add: APIs documentations

--- app/urls.py
+++ app/urls.py
@@ -18,9 +18,30 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Licitadoc API",
+      default_version='v1',
+      description="Documentação da API do Licitadoc",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="seuemail@dominio.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,  # Deixe False se quiser proteger o Swagger
+   permission_classes=(permissions.AllowAny,),  # Proteção do Swagger
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('documents/', include('manager_documents.urls')),
     path('auth/', include('custom_auth.urls')),
+     # Swagger UI:
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # Redoc UI (opcional):
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
